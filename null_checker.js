fc17c8451a2b5c8e6d5dd0861a176b9001be2b08
Peter Angusa <peter.angusa@outlook.com>
2019-03-19 15:17:33 +0300

    arrow checker

---
+++ null_checker.js
@@ -0,0 +1,17 @@
+const checker = (num) => {
+
+if (num>0) //if the value is greater than zero'
+{
+	console.log(1);
+}
+else if (num<0) //if less than zero'
+{
+	console.log(-1);
+}
+else
+{
+	console.log(0);//if equals zero'
+}
+return num;
+}
+checker(-2)
